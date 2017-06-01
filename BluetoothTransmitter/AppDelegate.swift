e8cc14d3a6fd8c53a44cf04b35630dbad67d96d5
natebirkholz <nbirkholz@gmail.com>
2017-06-01 07:17:31 -0700

    Initial Commit

---
+++ BluetoothTransmitter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BluetoothTransmitter
+//
+//  Created by Nathan Birkholz on 6/1/17.
+//  Copyright © 2017 natebirkholz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BluetoothTransmitter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BluetoothTransmitter
+//
+//  Created by Nathan Birkholz on 6/1/17.
+//  Copyright © 2017 natebirkholz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
