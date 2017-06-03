8c329925649ab533bacfd8d3f91c73cc63ef9980
natebirkholz <nbirkholz@gmail.com>
2017-06-02 23:27:49 -0700

    Cleanup

--- BluetoothTransmitter/ViewController.swift
+++ BluetoothTransmitter/ViewController.swift
@@ -14,44 +14,35 @@ class ViewController: NSViewController, CBPeripheralManagerDelegate {
     var label: NSTextView!
     var button: NSButton!
     var myServiceUUID: CBUUID!
-
     var myCharacteristic: CBCharacteristic!
-
     var bluetoothController : CBPeripheralManager!
-
     var subscriber: CBCentral!
-
     var dateFormatter = DateFormatter()
-
     var timer: Timer?
 
-
     override func viewDidLoad() {
+        super.viewDidLoad()
+
         let text = NSTextView(frame: NSRect(x: 20, y: 20, width: 200, height: 24))
         text.isEditable = false
-        text.string = "Running..."
+        text.string = "On hold..."
+        text.backgroundColor = NSColor.red.withAlphaComponent(0.33)
         label = text
         view.addSubview(text)
 
-        dateFormatter.dateFormat = "HH:mm:ss"
-
         let buttonFor = NSButton()
         buttonFor.title = "SEND"
-        buttonFor.action = #selector(advertise)
+        buttonFor.action = #selector(buttonClicked)
         buttonFor.target = self
         buttonFor.frame.origin = NSPoint(x: 20, y: 60)
         buttonFor.sizeToFit()
-
+        buttonFor.layer?.backgroundColor = NSColor.red.withAlphaComponent(0.33).cgColor
         view.addSubview(buttonFor)
         button = buttonFor
 
-
-
-        super.viewDidLoad()
+        dateFormatter.dateFormat = "HH:mm:ss"
         bluetoothController = CBPeripheralManager(delegate: self, queue: nil, options: nil)
         myServiceUUID = CBUUID(string: "D701F42C-49E1-48E9-B6E2-3862FEB2F550")
-
-
         let thisData = [CBAdvertisementDataServiceUUIDsKey: [myServiceUUID]]
         bluetoothController.startAdvertising(thisData)
     }
@@ -69,31 +60,26 @@ class ViewController: NSViewController, CBPeripheralManagerDelegate {
         bluetoothController.add(myService)
     }
 
-    func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveRead request: CBATTRequest) {
-        //
-    }
-
     func peripheralManager(_ peripheral: CBPeripheralManager, didAdd service: CBService, error: Error?) {
         if let error = error {
             print(error)
         }
     }
 
-    func peripheralManagerIsReady(toUpdateSubscribers peripheral: CBPeripheralManager) {
-        print("ready to update")
-    }
-
     func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
-        print(characteristic.uuid)
         subscriber = central
     }
 
     func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didUnsubscribeFrom characteristic: CBCharacteristic) {
-        print(characteristic)
-        print("unsubscribe")
+        print("--- !! unsubscribe !! ---")
+        // Stop pushing data and set button color appropriately
+        timer?.invalidate()
+        timer = nil
+        label.string = "On hold..."
+        label.backgroundColor = NSColor.red.withAlphaComponent(0.33)
     }
 
-    func repeatAdvertisement() {
+    func repeatTimestamp() {
         timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { [unowned self] (timerRef) in
             guard let maybeTimer = self.timer, maybeTimer.isValid else { return }
             let datum = Date()
@@ -106,19 +92,22 @@ class ViewController: NSViewController, CBPeripheralManagerDelegate {
             let did = self.bluetoothController.updateValue(data!, for: self.myCharacteristic as! CBMutableCharacteristic, onSubscribedCentrals: [self.subscriber])
             print("timed \(stringFromDate) \(did)")
         }
-
-
     }
 
-
-    func advertise() {
+    func buttonClicked() {
         if timer == nil {
-            repeatAdvertisement()
+            // Start pushing data. Also update button color to green to indicate running
+            repeatTimestamp()
+            label.string = "Transmitting..."
+            label.backgroundColor = NSColor.green.withAlphaComponent(0.33)
+
         } else {
+            // Stop pushing data and set button color
             timer?.invalidate()
             timer = nil
+            label.string = "On hold..."
+            label.backgroundColor = NSColor.red.withAlphaComponent(0.33)
         }
     }
-
 }
 
